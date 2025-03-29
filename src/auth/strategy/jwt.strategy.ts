6ea7ae74632bfbf2d6e960a9f442bc5c8b96d54e
Alexandra Bogdanova <alexandra.kaichev@gmail.com>
2025-03-29 22:52:40 +0100

    09 implement route authorization with token

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { AccessTokenPayload } from "../types";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy (
+  Strategy,
+  'jwt', // value by default
+) {
+  constructor(
+    config: ConfigService,
+    private prisma: PrismaService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET')
+    });
+  }
+
+  async validate(payload: AccessTokenPayload) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.sub,
+      },
+    });
+    delete user.hash;
+    return user;
+  }
+}
